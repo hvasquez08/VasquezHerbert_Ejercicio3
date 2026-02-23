4c08b1d17c01f1e4fb7e40b65422f39b5be7ccf2
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-02-22 19:04:39 -0600

    ingreso de datos

--- VasquezHerbert_Ejercicio3/Program.cs
+++ VasquezHerbert_Ejercicio3/Program.cs
@@ -1,1 +1,9 @@
 ﻿//Creacion del github
+Console.WriteLine("QUE ES USTED?");
+Console.WriteLine("Tipo de cliente\r\n1 Estudiante\r\n2 Docente\r\n3 Administrativo\r\n4 Externo");
+int cliente = int.Parse(Console.ReadLine());
+Console.WriteLine("Con que metodo paga?");
+Console.WriteLine("Monto base\r\nMétodo de pago\r\n1 Efectivo\r\n2 Tarjeta\r\n3 Transferencia");
+int pago = int.Parse(Console.ReadLine());
+Console.WriteLine("Tiene cupongo?");
+string cupo = Console.ReadLine();
